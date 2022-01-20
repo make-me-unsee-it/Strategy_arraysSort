e0fdd5a93ea684d2152b5e49644e3a64e95c9c06
alekx <alekx-gr@yandex.by>
2022-01-20 18:14:43 +0300

    selectionSort added

--- src/com/SelectionSort.java
+++ src/com/SelectionSort.java
@@ -1,8 +1,19 @@
 package com;
 
-public class SelectionSort implements Sorter{
+public class SelectionSort implements Sorter {
     @Override
-    public int[] sortArray(int[] array){
+    public int[] sortArray(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[index]) {
+                    index = j;
+                }
+            }
+            int smallerNumber = array[index];
+            array[index] = array[i];
+            array[i] = smallerNumber;
+        }
         return array;
     }
 }
